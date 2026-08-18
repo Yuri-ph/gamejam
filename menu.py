5842f94a9effd8529307b5e595f6ef00830479e5
Vinicios Alves <viniciosconte045@gmail.com>
2026-08-18 10:34:17 -0300

    Calculo de energia

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ import random
 dicas = [
     "Reduza, Recicle e Reutilize \n| Diminua o consumo de descartáveis, \n| use itens reutilizáveis e separe o lixo reciclável para evitar que materiais vão para aterros",
     "Economize água \n| Feche a torneira ao escovar os dentes, conserte vazamentos e instale redutores de vazão em chuveiros e torneiras",
-    "Economize energia \n |Desligue aparelhos quando não estiverem em uso, use lâmpadas LED e aproveite a luz natural "
+    "Economize energia \n| Desligue aparelhos quando não estiverem em uso, use lâmpadas LED e aproveite a luz natural "
 ]
 
 def menu():
