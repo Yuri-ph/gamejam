bc24a1991e7eaaa9e8e4f3b8ed847932f7fefc1c
Yuri <biancapierri@gmail.com>
2026-08-18 10:51:27 -0300

    Update carbono.py

--- carbono.py
+++ carbono.py
@@ -1,57 +1,54 @@
 Fator_Gasolina = 0.192
-
+ 
+ 
 def CalculoCarbono():
-
+ 
     print("=== Calculadora de carbono semanal (carro) ===")
     print()
-
-    km_dia = float(input("Quantos km você roda de carro por dia? "))
-    dias_semanas = float(input("Quantos dias por semana você usa o carro?"))
-
-    km_semana = km_dia * dias_semanas
+ 
+    while True:
+        try:
+            km_dia = float(input("Quantos km você roda de carro por dia? "))
+ 
+            if km_dia < 0:
+                print("Erro: o valor não pode ser negativo. Tente novamente.")
+                continue
+ 
+            break
+ 
+        except ValueError:
+            print("Erro: digite um número válido (ex: 10 ou 10.5).")
+ 
+ 
+    while True:
+        try:
+            dias_semana = float(input("Quantos dias por semana você usa o carro? "))
+ 
+            if dias_semana < 0 or dias_semana > 7:
+                print("Erro: o valor deve estar entre 0 e 7.")
+                continue
+ 
+            break
+ 
+        except ValueError:
+            print("Erro: digite um número válido (ex: 5).")
+ 
+ 
+    km_semana = km_dia * dias_semana
     co2_semana = km_semana * Fator_Gasolina
-
+ 
     print()
     print("--- Resultado ---")
     print("Km rodados na semana:", km_semana, "km")
-    print("CO2 emitido na semana:", co2_semana, "Kg")
-
+    print("CO2 emitido na semana:", co2_semana, "kg")
+ 
+ 
 while True:
-    try:
-        km_dia = float(input("Quantos Km você roda de carro por dia? "))
-        if km_dia <0:
-            print("Erro: o valor não pode ser negativo. Tente novamente.")
-            continue 
-        break
-    except ValueError:
-        print("Erro: digite um número válido (ex: 10 ou 10.5).")
-
-while True:
-    try:
-        dias_semana = float(input("Quantos dias na semana você usa o carro? "))
-        if dias_semana < 0 or dias_semana > 7:
-            print("Erro: o valor deve estar entre 0 e 7. Tente novamente. ")
-            continue
-        break
-    except ValueError:
-        print("Erro: digite um número válido (ex: 5).")
-        continue
-
-
-
-km_semana = km_dia * dias_semana
-co2_semana = km_semana * Fator_Gasolina
-
-
-print()
-print("--- Resultado ---")
-print("Km rodados na semana:", km_semana, "km")
-print("CO2 emitido na semana:", co2_semana, "kg")
-
-while True:
+ 
     CalculoCarbono()
-
-    continuar = input("\n Deseja fazer outro cálculo? (sim/não)").strip().lower()
-    if continuar != "nao":
-            print("Encerrando o programa...")
-            break
+ 
+    continuar = input("\nDeseja fazer outro cálculo? (sim/não): ").strip().lower()
+ 
+    if continuar == "não" or continuar == "nao":
+        print("Encerrando o programa...")
+        break
