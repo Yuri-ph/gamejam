d166daf7d181d2abe8c6f49f16444d1c3047adbd
Yuri <biancapierri@gmail.com>
2026-08-18 08:28:13 -0300

    Update carbono.py

--- carbono.py
+++ carbono.py
@@ -1,16 +1,17 @@
-FATOR_GASOLINA = 0.192
-print("=== Calculadora de carbono semanal (carro) ===")
-print()
+def CalculoCarbono():
+    FATOR_GASOLINA = 0.192
+    print("=== Calculadora de carbono semanal (carro) ===")
+    print()
 
-km_dia = float(input("Quantos km você roda de carro por dia?"))
+    km_dia = float(input("Quantos km você roda de carro por dia? "))
+    dias_semanas = float(input("Quantos dias por semana você usa o carro?"))
 
-dias_semanas = float(input("Quantos dias por semana você usa o carro? "))
+    km_semana = km_dia * dias_semanas
+    co2_semana = km_semana * FATOR_GASOLINA
 
-km_semana = km_dia * dias_semanas
+    print()
+    print("--- Resultado ---")
+    print("Km rodados na semana:", km_semana, "km")
+    print("CO2 emitido na semana:", co2_semana, "Kg")
 
-co2_semana = km_dia * FATOR_GASOLINA
-
-print()
-print("--- Resultado ---")
-print(f"Km rodados na semana: {km_semana:.1f} km")
-print(f"CO2 emitido na semana: {co2_semana:.1f} kg")
+CalculoCarbono()
