24438e0822e0726efdb1f3e567edb7933216d3ab
Péres <davi5211peres@gmail.com>
2026-08-18 10:51:39 -0300

    Implement energy calculation menu
    Added a menu for energy calculations including options for shower and air conditioning energy usage.

--- energia.py
+++ energia.py
@@ -1,3 +1,21 @@
+
+def menuEnergia():
+    while True:
+        print("\n __________MENU_DE_ENERGIA__________")
+        print("|")
+        print("| 1- Calcular energia do banho")
+        print("| 2- Calcular energia do ar condicionado")
+        print("|")
+        print("|_____________________________________")
+        
+        escolhaMenu = input("| qual sera sua escolha: ")
+        
+        if escolhaMenu == "1":
+            CalculoBanho()
+            
+        if escolhaMenu == "2":
+            CalculoAr()
+            
 def CalculoBanho():
     while True:
         banhosdiarios = input("Quantos banhos você toma por dia: ")
@@ -36,5 +54,4 @@ def CalculoBanho():
 CalculoBanho()
 
 def CalculoAr():
-        while True:
-             artempo = input("")
+    print("zezé")
