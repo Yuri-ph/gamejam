59da50384652d685c2c093b437702da55a63cae1
Péres <davi5211peres@gmail.com>
2026-08-18 09:36:29 -0300

    dicas

--- menu.py
+++ menu.py
@@ -1,19 +1,29 @@
 import time
 import random
 dicas = [
-    "dica 1",
-    "dica 2",
-    "dica 3"
+    "Reduza, Recicle e Reutilize \n| Diminua o consumo de descartáveis, \n| use itens reutilizáveis e separe o lixo reciclável para evitar que materiais vão para aterros",
+    "Economize água \n| Feche a torneira ao escovar os dentes, conserte vazamentos e instale redutores de vazão em chuveiros e torneiras",
+    "Economize energia \n |Desligue aparelhos quando não estiverem em uso, use lâmpadas LED e aproveite a luz natural "
 ]
 
 def menu():
     while True:
         dica = random.choice(dicas)
-        print(" __________MENU__________")
+        print("\n __________MENU__________")
+        print("|")
         print("|")
         print("| 1- ")
         print("| 2- ")
         print("| 3- ")
-        print("| 💡", dica)
+        print("|💡 DICA: ", dica)
+        print("|")
+        print("|________________________")
+        
+        escolha == input("qual sera sua escolha: ")
+        
+        if escolha == "1":
+            print("")
+
+
 
 menu()
