4da0733eecb5b11daf612e5f9799fc4c2da14a87
Péres <davi5211peres@gmail.com>
2026-08-18 09:03:06 -0300

    teste do funcionamento das dicas

--- menu.py
+++ menu.py
@@ -1,5 +1,19 @@
+import time
+import random
+dicas = [
+    "dica 1",
+    "dica 2",
+    "dica 3"
+]
+
 def menu():
-    print(" __________MENU__________")
-    print("|")
-    
+    while True:
+        dica = random.choice(dicas)
+        print(" __________MENU__________")
+        print("|")
+        print("| 1- ")
+        print("| 2- ")
+        print("| 3- ")
+        print("| 💡", dica)
+
 menu()
