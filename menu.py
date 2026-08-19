b9649a2301e084a5e356aa00678a223c8025c14d
Péres <davi5211peres@gmail.com>
2026-08-19 10:25:08 -0300

    .

--- menu.py
+++ menu.py
@@ -2,15 +2,14 @@ import time
 import random
 import os
 from agua import *
-from carbono import *
+# from carbono import *
 from energia import *
 dicas = [
-    "Reduza, Recicle e Reutilize \n| Diminua o consumo de descartáveis, \n| use itens reutilizáveis e separe o lixo reciclável para evitar que materiais vão para aterros",
-    "Economize água \n| Feche a torneira ao escovar os dentes, conserte vazamentos e instale redutores de vazão em chuveiros e torneiras",
-    "Economize energia \n| Desligue aparelhos quando não estiverem em uso, use lâmpadas LED e aproveite a luz natural ",
-    "Economize energia \n| Desligue aparelhos quando não estiverem em uso, use lâmpadas LED e aproveite a luz natural ",
-    "Opte por transporte sustentável \n| Prefira bicicleta, transporte público, caronas ou veículos elétricos para reduzir emissões",
-    "Consuma de forma consciente \n| Compre produtos locais, orgânicos e com embalagens mínimas; evite marcas que não adotem práticas sustentáveis"
+    "Reduza, Recicle e Reutilize ♻️ \n| Diminua o consumo de descartáveis, \n| use itens reutilizáveis e separe o lixo reciclável para evitar que materiais vão para aterros",
+    "Economize água 🫗 \n| Feche a torneira ao escovar os dentes, conserte vazamentos e instale redutores de vazão em chuveiros e torneiras",
+    "Economize energia 💡 \n| Desligue aparelhos quando não estiverem em uso, use lâmpadas LED e aproveite a luz natural ",
+    "Opte por transporte sustentável 🚲 \n| Prefira bicicleta, transporte público, caronas ou veículos elétricos para reduzir emissões",
+    "Consuma de forma consciente 🍎 \n| Compre produtos locais, orgânicos e com embalagens mínimas; evite marcas que não adotem práticas sustentáveis"
 ]
 
 def menu():
@@ -23,7 +22,7 @@ def menu():
         print("| 2- Calculo de gasto de energia")
         print("| 3- Calculo de gasto de caborno")
         print("|")
-        print("|💡 DICA: ", dica)
+        print("| DICA: ", dica)
         print("|")
         print("|________________________")
         
@@ -35,8 +34,8 @@ def menu():
         if escolhaMenu == "2":
             menuEnergia()
             
-        if escolhaMenu == "3":
-            CalculoCarbono()
+        # if escolhaMenu == "3":
+        #     CalculoCarbono()
 
         else:
             print("| Opção inválida!")
